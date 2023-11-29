bc7cd5e285e240d8c575225b45f8a93ce53e5d81
Patrick Shriwise <pshriwise@gmail.com>
2023-11-28 22:14:40 -0600

    Adding empty classes and initial namespace

---
+++ include/xdg/mesh_manager_interface.h
@@ -0,0 +1,12 @@
+
+#ifndef _XDG_MESH_MANAGER_INTERFACE
+#define _XDG_MESH_MANAGER_INTERFACE
+
+namespace xdg {
+class MeshManager {
+
+};
+
+} // namespace xdg
+
+#endif
--- include/xdg/moab/mesh_manager.h
+++ include/xdg/moab/mesh_manager.h
@@ -1,4 +1,15 @@
+#ifndef _XDG_MOAB_MESH_MANAGER
+#define _XDG_MOAB_MESH_MANAGER
 
 #include "xdg/mesh_manager_interface.h"
 
+
 #include "moab/Core.hpp"
+
+using namespace xdg;
+
+class MOABMeshManager : MeshManager {
+
+};
+
+#endif
