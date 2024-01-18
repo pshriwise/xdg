de5d177069dfd98a5511ada6f66e4e1ac59a9f5b
Patrick Shriwise <pshriwise@gmail.com>
2024-01-18 12:49:43 -0600

    Beefing up the particle class a bit

--- tools/particle_sim.cpp
+++ tools/particle_sim.cpp
@@ -11,10 +11,68 @@
 using namespace xdg;
 
 struct Particle {
-  Position r;
-  Direction u;
-  MeshID volume;
-  std::vector<MeshID> history;
+
+  Particle(std::shared_ptr<XDG> xdg) : xdg_(xdg) {}
+
+  void initialize() {
+    // TODO: replace with sampling
+    r_ = {0.0, 0.0, 0.0};
+    u_ = {1.0, 0.0, 0.0};
+
+    volume_ = xdg_->find_volume(r_, u_);
+  }
+
+  void surf_dist() {
+    surface_intersection_ = xdg_->ray_fire(volume_, r_, u_, &history_);
+    if (surface_intersection_.second == ID_NONE) {
+      std::cerr << "Particle lost in volume " << volume_ << std::endl;
+      alive_ = false;
+      return;
+    }
+    std::cout << "Intersected surface " << surface_intersection_.second << " at distance " << surface_intersection_.first << std::endl;
+  }
+
+  void sample_collision_distance() { collision_distance_ = INFTY; }
+
+  void collide() {
+    n_events_++;
+    std::cout << "Particle collides with material at position " << r_ << std::endl;
+  }
+
+  void advance() {
+    if (collision_distance_ < surface_intersection_.first) {
+      r_ += collision_distance_ * u_;
+      std::cout << "Particle collides with material at position " << r_ << std::endl;
+    } else {
+      std::cout << "Particle advances to surface " << surface_intersection_.second << " at position " << r_ << std::endl;
+      r_ += surface_intersection_.first * u_;
+    }
+  }
+
+  void cross_surface() {
+    n_events_++;
+
+    volume_ = xdg_->mesh_manager()->next_volume(volume_, surface_intersection_.second);
+    std::cout << "Particle enters volume " << volume_ << std::endl;
+    if (volume_ == ID_NONE) {
+      alive_ = false;
+      return;
+    }
+  }
+
+  // Data Members
+  std::shared_ptr<XDG> xdg_;
+
+  Position r_;
+  Direction u_;
+  MeshID volume_ {ID_NONE};
+  std::vector<MeshID> history_{};
+
+  std::pair<double, MeshID> surface_intersection_ {INFTY, ID_NONE};
+  double collision_distance_ {INFTY};
+
+  int32_t n_events_ {0};
+  bool alive_ {true};
 };
 
 int main(int argc, char** argv) {
@@ -30,24 +88,22 @@ mm->init();
 xdg->prepare_raytracer();
 
 // create a new particle
-Particle p{ {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {ID_NONE}, {}};
+Particle p(xdg);
 
-// determine what volume this particle is in
-p.volume = xdg->find_volume(p.r, p.u);
-
-std::cout << "Particle starts in volume " << p.volume << std::endl;
-
-std::pair<double, MeshID> intersection;
-
-intersection = xdg->ray_fire(p.volume, p.r, p.u, &p.history);
-
-std::cout << "Intersected surface " << intersection.second << " at distance " << intersection.first << std::endl;
-
-p.volume = xdg->mesh_manager()->next_volume(p.volume, intersection.second);
-p.r += intersection.first * p.u;
-
-std::cout << "Particle enters volume " << p.volume << " at position " << p.r << std::endl;
+const int max_events {10};
+p.initialize();
+while (p.n_events_ < max_events) {
+  p.surf_dist();
+  // terminate for leakage
+  if (!p.alive_) break;
+  p.sample_collision_distance();
+  p.advance();
+  if (p.surface_intersection_.first < p.collision_distance_)
+    p.cross_surface();
+  else
+    p.collide();
+  if (!p.alive_) break;
+}
 
 return 0;
-
 }
