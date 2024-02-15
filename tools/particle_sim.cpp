b56dcb91e838f78aa3cda844e1b29f1407b76d36
Patrick Shriwise <pshriwise@gmail.com>
2024-02-15 17:33:40 -0600

    Adding particle IDs. Setting random seed

--- tools/particle_sim.cpp
+++ tools/particle_sim.cpp
@@ -14,7 +14,7 @@ static double mfp {1.0};
 
 struct Particle {
 
-Particle(std::shared_ptr<XDG> xdg) : xdg_(xdg) {}
+Particle(std::shared_ptr<XDG> xdg, uint32_t id) : xdg_(xdg), id_(id) {}
 
 void initialize() {
   // TODO: replace with sampling
@@ -27,7 +27,7 @@ void initialize() {
 void surf_dist() {
   surface_intersection_ = xdg_->ray_fire(volume_, r_, u_, &history_);
   if (surface_intersection_.second == ID_NONE) {
-    std::cerr << "Particle lost in volume " << volume_ << std::endl;
+    std::cerr << "Particle " << id_ << " lost in volume " << volume_ << std::endl;
     alive_ = false;
     return;
   }
@@ -41,7 +41,7 @@ void sample_collision_distance() {
 void collide() {
   n_events_++;
   u_ = rand_dir();
-  std::cout << "Particle collides with material at position " << r_ << ", new direction is " << u_ << std::endl;
+  std::cout << "Particle " << id_ << " collides with material at position " << r_ << ", new direction is " << u_ << std::endl;
   history_.clear();
 }
 
@@ -49,10 +49,10 @@ void advance()
 {
   if (collision_distance_ < surface_intersection_.first) {
     r_ += collision_distance_ * u_;
-    std::cout << "Particle collides with material at position " << r_ << std::endl;
+    std::cout << "Particle " << id_ << " collides with material at position " << r_ << std::endl;
   } else {
     r_ += surface_intersection_.first * u_;
-    std::cout << "Particle advances to surface " << surface_intersection_.second << " at position " << r_ << std::endl;
+    std::cout << "Particle " << id_ << " advances to surface " << surface_intersection_.second << " at position " << r_ << std::endl;
   }
 }
 
@@ -61,14 +61,14 @@ void cross_surface()
   n_events_++;
   // check for the surface boundary condition
   if (xdg_->mesh_manager()->get_surface_property(surface_intersection_.second, PropertyType::BOUNDARY_CONDITION).value == "reflecting") {
-    std::cout << "Particle reflects off surface " << surface_intersection_.second << std::endl;
+    std::cout << "Particle " << id_ << " reflects off surface " << surface_intersection_.second << std::endl;
     Direction normal = xdg_->surface_normal(surface_intersection_.second, r_, &history_);
     u_ = u_ - 2.0 * dot(u_, normal) * normal;
     u_ = u_.normalize();
     if (history_.size() > 0) history_ = {history_.back()}; // reset to last intersection
   } else {
     volume_ = xdg_->mesh_manager()->next_volume(volume_, surface_intersection_.second);
-    std::cout << "Particle enters volume " << volume_ << std::endl;
+    std::cout << "Particle " << id_ << " enters volume " << volume_ << std::endl;
     if (volume_ == ID_NONE) {
       alive_ = false;
       return;
@@ -78,7 +78,7 @@ void cross_surface()
 
 // Data Members
 std::shared_ptr<XDG> xdg_;
-
+uint32_t id_ {0};
 Position r_;
 Direction u_;
 MeshID volume_ {ID_NONE};
@@ -93,6 +93,8 @@ bool alive_ {true};
 
 int main(int argc, char** argv) {
 
+srand48(42);
+
 // create a mesh manager
 std::shared_ptr<XDG> xdg = XDG::create(MeshLibrary::MOAB);
 const auto& mm = xdg->mesh_manager();
@@ -105,7 +107,7 @@ mm->parse_metadata();
 xdg->prepare_raytracer();
 
 // create a new particle
-Particle p(xdg);
+Particle p(xdg, 1);
 
 const int max_events {100};
 p.initialize();
