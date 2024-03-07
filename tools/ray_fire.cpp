f06d9da150a10899075d3118c5477c4db1914b94
Patrick Shriwise <pshriwise@gmail.com>
2024-03-07 16:54:10 -0600

    Adding a ray fire tool

---
+++ tools/ray_fire.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "xdg/error.h"
+#include "xdg/mesh_manager_interface.h"
+#include "xdg/moab/mesh_manager.h"
+#include "xdg/vec3da.h"
+#include "xdg/xdg.h"
+
+#include "argparse/argparse.hpp"
+
+using namespace xdg;
+
+int main(int argc, char** argv) {
+
+  argparse::ArgumentParser args("XDG Ray Fire Tool", "1.0", argparse::default_arguments::help);
+
+  args.add_argument("filename")
+    .help("Path to the input file");
+
+  args.add_argument("volume")
+    .help("Volume ID to query").scan<'i', int>();
+
+  args.add_argument("-l", "--list")
+    .default_value(false)
+    .implicit_value(true)
+    .help("List all volumes in the file and exit");
+
+  args.add_argument("-o", "--origin")
+    .default_value(std::vector<double>{0.0, 0.0, 0.0})
+    .help("Ray origin").scan<'g', double>().nargs(3);
+
+  args.add_argument("-d", "--direction")
+    .default_value(std::vector<double>{0.0, 0.0, 1.0})
+    .help("Ray direction").scan<'g', double>().nargs(3);
+
+  try {
+    args.parse_args(argc, argv);
+  }
+  catch (const std::runtime_error& err) {
+    std::cout << err.what() << std::endl;
+    std::cout << args;
+    exit(0);
+  }
+
+
+  // create a mesh manager
+  std::shared_ptr<XDG> xdg = XDG::create(MeshLibrary::MOAB);
+  const auto& mm = xdg->mesh_manager();
+  mm->load_file(args.get<std::string>("filename"));
+  mm->init();
+  mm->parse_metadata();
+  xdg->prepare_raytracer();
+
+  if (args.get<bool>("--list")) {
+    std::cout << "Volumes: " << std::endl;
+    for (auto volume : mm->volumes()) {
+      std::cout << volume << std::endl;
+    }
+    exit(0);
+  }
+
+  MeshID volume = args.get<int>("volume");
+  Position origin = args.get<std::vector<double>>("--origin");
+  Direction direction = args.get<std::vector<double>>("--direction");
+
+  auto result = xdg->ray_fire(volume, origin, direction);
+
+  std::cout << "Distance: " << result.first << std::endl;
+  std::cout << "Surface: " << result.second << std::endl;
+
+  return 0;
+}
