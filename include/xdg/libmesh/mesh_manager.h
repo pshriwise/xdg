df9b66c21f3fc55e14aa5ae4a7676c2215d3e8ea
Patrick Shriwise <pshriwise@gmail.com>
2024-04-10 06:52:43 -0500

    Adding init to mesh manager

--- include/xdg/libmesh/mesh_manager.h
+++ include/xdg/libmesh/mesh_manager.h
@@ -1,9 +1,14 @@
 #ifndef _XDG_LIBMESH_MESH_MANAGER
 #define _XDG_LIBMESH_MESH_MANAGER
 
+#include <memory>
+
 #include "xdg/mesh_manager_interface.h"
 
+#include "libmesh/libmesh.h"
 namespace xdg {
+
+std::unique_ptr<libMesh::LibMeshInit> libmesh_init {nullptr};
 
 class LibMeshMeshManager : public MeshManager {
 
