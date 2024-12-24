870c5a80ae544c6316a43113a1d18c2d5663889c
Patrick Shriwise <pshriwise@gmail.com>
2024-12-23 22:02:24 -0600

    Moving transport stuff into header.

--- tools/particle_sim.h
+++ tools/particle_sim.h
@@ -9,11 +9,8 @@
 #include "xdg/vec3da.h"
 #include "xdg/xdg.h"
 
-#include "argparse/argparse.hpp"
-
-#include "particle_sim.h"
-
 using namespace xdg;
+
 
 struct SimulationData {
   std::shared_ptr<XDG> xdg_;
@@ -44,7 +41,7 @@ void initialize() {
 }
 
 void surf_dist() {
-  surface_intersection_ = xdg_->ray_fire(volume_, r_, u_, INFTY, &history_);
+  surface_intersection_ = xdg_->ray_fire(volume_, r_, u_, &history_);
   if (surface_intersection_.first == 0.0) {
     fatal_error("Particle {} stuck at position ({}, {}, {}) on surfacce {}", id_, r_.x, r_.y, r_.z, surface_intersection_.second);
     alive_ = false;
@@ -153,74 +150,4 @@ void transport_particles(SimulationData& sim_data) {
       }
     }
   }
-}
-
-
-int main(int argc, char** argv) {
-
-// argument parsing
-argparse::ArgumentParser args("XDG Particle Pseudo-Simulation", "1.0", argparse::default_arguments::help);
-
-args.add_argument("filename")
-    .help("Path to the input file");
-
-args.add_argument("-v", "--verbose")
-    .default_value(false)
-    .implicit_value(true)
-    .help("Enable verbose output of particle events");
-
-args.add_argument("-m", "--mfp")
-    .default_value(1.0)
-    .help("Mean free path of the particles").scan<'g', double>();
-
-args.add_argument("-l", "--library")
-    .help("Mesh library to use. One of (MOAB, LIBMESH)")
-    .default_value("MOAB");
-
-  try {
-    args.parse_args(argc, argv);
-  }
-  catch (const std::runtime_error& err) {
-    std::cout << err.what() << std::endl;
-    std::cout << args;
-    exit(0);
-  }
-
-// Problem Setup
-srand48(42);
-
-SimulationData sim_data;
-
-// create a mesh manager
-std::shared_ptr<XDG> xdg {nullptr};
-if (args.get<std::string>("--library") == "MOAB")
-  xdg = XDG::create(MeshLibrary::MOAB);
-else if (args.get<std::string>("--library") == "LIBMESH")
-  xdg = XDG::create(MeshLibrary::LIBMESH);
-
-sim_data.xdg_ = xdg;
-
-const auto& mm = xdg->mesh_manager();
-mm->load_file(args.get<std::string>("filename"));
-mm->init();
-mm->parse_metadata();
-xdg->prepare_raytracer();
-
-// update the mean free path
-sim_data.mfp_ = args.get<double>("--mfp");
-
-sim_data.verbose_particles_ = args.get<bool>("--verbose");
-
-transport_particles(sim_data);
-
-// report distances in each cell in a table
-write_message("Cell Track Lengths");
-write_message("-----------");
-for (const auto& [cell, dist] : sim_data.cell_tracks) {
-  write_message("Cell {}: {}", cell, dist);
-}
-write_message("-----------");
-
-
-return 0;
 }
