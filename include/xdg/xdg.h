687d5bf672a8c55e0022aa125a1d3000fc56a034
waqar-ukaea <waqar.butt@ukaea.uk>
2025-02-12 13:57:54 +0000

    Fixed broken ray_fire definition

--- include/xdg/xdg.h
+++ include/xdg/xdg.h
@@ -37,7 +37,8 @@ std::pair<double, MeshID> ray_fire(MeshID volume,
                                    const Position& origin,
                                    const Direction& direction,
                                    const double dist_limit = INFTY,
-                                   std::vector<MeshID>* const exclude_primitives = nullptr) const;
+                                   std::vector<MeshID>* const exclude_primitives = nullptr,
+                                   HitOrientation orientation = HitOrientation::EXITING) const;
 
 void closest(MeshID volume,
               const Position& origin,
--- src/xdg.cpp
+++ src/xdg.cpp
@@ -60,10 +60,11 @@ XDG::ray_fire(MeshID volume,
               const Position& origin,
               const Direction& direction,
               const double dist_limit,
-              std::vector<MeshID>* const exclude_primitives) const
+              std::vector<MeshID>* const exclude_primitives,
+              HitOrientation orientation) const
 {
   TreeID scene = volume_to_scene_map_.at(volume);
-  return ray_tracing_interface()->ray_fire(scene, origin, direction, dist_limit, exclude_primitives);
+  return ray_tracing_interface()->ray_fire(scene, origin, direction, dist_limit, exclude_primitives, orientation);
 }
 
 void XDG::closest(MeshID volume,
