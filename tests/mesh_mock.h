be9562e35941e076cbf88a1df441c748afacfa18
Patrick Shriwise <pshriwise@gmail.com>
2025-04-02 16:20:48 -0500

    Cleaning up the MeshMock class a bit

--- tests/mesh_mock.h
+++ tests/mesh_mock.h
@@ -17,10 +17,11 @@ public:
     surfaces_ = {0, 1, 2, 3, 4, 5};
   }
 
+  // Required overloads
   void load_file(const std::string& file_name) override {}
-
   void init() override {}
 
+  // Counts
   virtual int num_volumes() const override {
     return 1;
   }
@@ -58,6 +59,7 @@ public:
     return 2;
   }
 
+  // Lists
   virtual std::vector<MeshID> get_volume_elements(MeshID volume) const override {
     if (!volumetric_elements_) return {};
     return {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // returning all tetrahedron elements
@@ -68,15 +70,16 @@ public:
     return {start, start + 1};
   }
 
+  // Coordinates
   virtual std::vector<Vertex> element_vertices(MeshID element) const override {
     if (!volumetric_elements_) return {};
-    const auto& conn = tetrahedron_connectivity[element];
-    return {vertices[conn[0]], vertices[conn[1]], vertices[conn[2]], vertices[conn[3]]};
+    const auto& conn = tetrahedron_connectivity()[element];
+    return {vertices()[conn[0]], vertices()[conn[1]], vertices()[conn[2]], vertices()[conn[3]]};
   }
 
   virtual std::array<Vertex, 3> face_vertices(MeshID element) const override {
-    const auto& conn = triangle_connectivity[element];
-    return {vertices[conn[0]], vertices[conn[1]], vertices[conn[2]]};
+    const auto& conn = triangle_connectivity()[element];
+    return {vertices()[conn[0]], vertices()[conn[1]], vertices()[conn[2]]};
   }
 
   // Topology
@@ -116,7 +119,7 @@ public:
                 const Position& u) const
   {
     // get the tetrahedron element
-    const auto& elem_ref = tetrahedron_connectivity[current_element];
+    const auto& elem_ref = tetrahedron_connectivity()[current_element];
 
     std::array<double, 4> dists = {INFTY, INFTY, INFTY, INFTY};
     std::array<bool, 4> hit_types = {false, false, false, false};
@@ -126,7 +129,7 @@ public:
       // triangle connectivity
       std::array<Position, 3> coords;
       for (int j = 0; j < 3; j++) {
-        coords[j] = vertices[elem_ref[(i + j) % 4]];
+        coords[j] = vertices()[elem_ref[(i + j) % 4]];
       }
       // get the normal of the triangle
       const Position v1 = coords[1] - coords[0];
@@ -154,7 +157,7 @@ public:
       fatal_error(fmt::format("No exit found in element {}", current_element));
     }
 
-    MeshID next_element = element_adjacencies.at(current_element)[idx_out];
+    MeshID next_element = element_adjacencies().at(current_element)[idx_out];
     return {next_element, min_dist};
  }
 
@@ -179,6 +182,22 @@ public:
     return bounding_box_;
   }
 
+  const std::vector<Vertex>& vertices() const {
+    return vertices_;
+  }
+
+  const std::vector<std::array<int, 3>>& triangle_connectivity() const {
+    return triangle_connectivity_;
+  }
+
+  const std::vector<std::array<int, 4>>& tetrahedron_connectivity() const {
+    return tetrahedron_connectivity_;
+  }
+
+  const std::unordered_map<MeshID, std::array<MeshID, 4>>& element_adjacencies() const {
+    return element_adjacencies_;
+  }
+
   // Other
   virtual MeshLibrary mesh_library() const override { return MeshLibrary::INTERNAL; }
 
@@ -188,7 +207,7 @@ private:
 
   const BoundingBox bounding_box_ {-2.0, -3.0, -4.0, 5.0, 6.0, 7.0};
 
-  const std::vector<Position> vertices {
+  const std::vector<Vertex> vertices_ {
     // vertices in the upper z plane
     {bounding_box_.max_x, bounding_box_.min_y, bounding_box_.max_z},
     {bounding_box_.max_x, bounding_box_.max_y, bounding_box_.max_z},
@@ -202,7 +221,26 @@ private:
     {bounding_box_.center()} // bounding box center for tet elements
   };
 
-  const std::vector<std::array<int, 3>> triangle_connectivity {
+    // tetrahedron connectivity for a cube, all elements connect to the center vertex
+  // there are 12 tetrahedron in total
+  const std::vector<std::array<int, 4>> tetrahedron_connectivity_ {
+    // each tetrahedron is defined by 4 vertices, the last vertex is the center of the bounding box
+    {0, 1, 2, 8}, // lower z plane
+    {0, 2, 3, 8},
+    {4, 6, 5, 8}, // upper z plane
+    {4, 7, 6, 8},
+    {0, 5, 1, 8}, // lower x plane
+    {0, 4, 5, 8},
+    {2, 6, 7, 8}, // upper x plane
+    {2, 7, 3, 8},
+    {0, 7, 4, 8}, // lower y plane
+    {0, 3, 7, 8},
+    {1, 5, 6, 8}, // upper y plane
+    {1, 6, 2, 8}
+  };
+
+  // triangle connectivity for the exterior faces of the tetrahedron above
+  const std::vector<std::array<int, 3>> triangle_connectivity_ {
     {0, 1, 2},
     {0, 2, 3},
     {4, 6, 5},
@@ -217,38 +255,19 @@ private:
     {1, 6, 2}
   };
 
-  // tetrahedron connectivity for a cube, all elements connect to the center vertex
-  // there are 12 tetrahedron in total
-  const std::vector<std::array<int, 4>> tetrahedron_connectivity {
-    // each tetrahedron is defined by 4 vertices, the last vertex is the center of the bounding box
-    {0, 1, 2, 8}, // lower z plane
-    {0, 2, 3, 8},
-    {4, 6, 5, 8}, // upper z plane
-    {4, 7, 6, 8},
-    {0, 5, 1, 8}, // lower x plane
-    {0, 4, 5, 8},
-    {2, 6, 7, 8}, // upper x plane
-    {2, 7, 3, 8},
-    {0, 7, 4, 8}, // lower y plane
-    {0, 3, 7, 8},
-    {1, 5, 6, 8}, // upper y plane
-    {1, 6, 2, 8}
-    };
-
-    std::unordered_map<MeshID, std::array<MeshID, 4>> element_adjacencies {
-      {0,  {-1, 1, 4, 11}},
-      {1,  {-1, 9, 0, 7}},
-      {2,  {-1, 5, 3, 10}},
-      {3,  {-1, 2, 8, 6}},
-      {4,  {-1, 0, 5, 10}},
-      {5,  {-1, 4, 8, 2}},
-      {6,  {-1, 7, 11, 3}},
-      {7,  {-1, 1, 6, 9}},
-      {8,  {-1, 5, 9, 3}},
-      {9,  {-1, 8, 1, 7}},
-      {10, {-1, 11, 4, 2}},
-      {11, {-1, 0, 10, 6}}
-    };
-
-
+  // face-adjacency list for the tetrahedron elements
+  std::unordered_map<MeshID, std::array<MeshID, 4>> element_adjacencies_ {
+    {0, {-1, 1, 4, 11}},
+    {1, {-1, 9, 0, 7}},
+    {2, {-1, 5, 3, 10}},
+    {3, {-1, 2, 8, 6}},
+    {4, {-1, 0, 5, 10}},
+    {5, {-1, 4, 8, 2}},
+    {6, {-1, 7, 11, 3}},
+    {7, {-1, 1, 6, 9}},
+    {8, {-1, 5, 9, 3}},
+    {9, {-1, 8, 1, 7}},
+    {10, {-1, 11, 4, 2}},
+    {11, {-1, 0, 10, 6}}
+  };
 };
