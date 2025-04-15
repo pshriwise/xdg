408a94185147ea89c1cffc9a19b96347752a94a5
waqar-ukaea <waqar.butt@ukaea.uk>
2025-04-15 10:19:16 +0100

    Added methods to return moab surface vertices and connectivity

--- include/xdg/moab/mesh_manager.h
+++ include/xdg/moab/mesh_manager.h
@@ -64,6 +64,10 @@ public:
 
   std::array<Vertex, 3> face_vertices(MeshID element) const override;
 
+  std::vector<int> get_surface_connectivity(MeshID surface) const;
+
+  std::vector<double> get_surface_vertices(MeshID surface) const;
+
   // Topology
   std::pair<MeshID, MeshID> surface_senses(MeshID surface) const override;
 
