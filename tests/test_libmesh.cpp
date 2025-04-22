e164441fec75ecdd5de8d1b6146eedb98add4094
Patrick Shriwise <pshriwise@gmail.com>
2025-04-22 14:00:40 -0500

    Adding a couple more libMesh tests

--- tests/test_libmesh.cpp
+++ tests/test_libmesh.cpp
@@ -29,6 +29,12 @@ TEST_CASE("Test Brick")
 
   REQUIRE(mesh_manager->num_volumes() == 1);
   REQUIRE(mesh_manager->num_surfaces() == 1);
+
+  // get a random element from volume 1
+  auto elements = mesh_manager->get_volume_elements(1);
+  REQUIRE(!elements.empty());
+  MeshID random_element = elements[0];
+  REQUIRE(mesh_manager->element_volume_id(random_element) == 1);
 }
 
 
@@ -113,6 +119,16 @@ TEST_CASE("Test Cylinder-Brick Initialization")
   REQUIRE(mesh_manager->num_volumes() == 2);
 
   REQUIRE(mesh_manager->num_surfaces() == 12);
+
+  // get an element from each volume and check its volume ID
+  auto vol1_elems = mesh_manager->get_volume_elements(1);
+  REQUIRE(!vol1_elems.empty());
+  REQUIRE(mesh_manager->element_volume_id(vol1_elems[0]) == 1);
+
+  auto vol2_elems = mesh_manager->get_volume_elements(2);
+  REQUIRE(!vol2_elems.empty());
+  REQUIRE(mesh_manager->element_volume_id(vol2_elems[0]) == 2);
+
 
   mesh_manager->parse_metadata();
 
