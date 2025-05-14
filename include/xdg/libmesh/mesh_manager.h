de1b9aaed0dee32686cb2782e977af82c29fa120
Patrick Shriwise <pshriwise@gmail.com>
2025-05-13 20:34:08 -0500

    Use incremental sidepair IDs

--- include/xdg/libmesh/mesh_manager.h
+++ include/xdg/libmesh/mesh_manager.h
@@ -277,11 +277,8 @@ public:
     return mesh_id_to_sidepair_.at(sidepair);
   }
 
-  MeshID next_sidepair_id() const {
-    if (mesh_id_to_sidepair_.size() == 0) {
-      return 1;
-    }
-    return std::max_element(mesh_id_to_sidepair_.begin(), mesh_id_to_sidepair_.end())->first + 1;
+  MeshID next_sidepair_id() {
+    return next_sidepair_id_++;
   }
 
   struct MeshIDPairHash {
@@ -322,6 +319,8 @@ public:
   //! Mapping of surfaces to the volumes on either side. Volumes are ordered
   //! based on their sense with respect to the surface triangles
   std::unordered_map<MeshID, std::pair<MeshID, MeshID>> surface_senses_;
+
+  int32_t next_sidepair_id_;
 };
 
 } // namespace xdg
