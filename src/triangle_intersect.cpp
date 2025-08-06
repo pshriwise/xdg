96aabff00efcfc069c85c3226f4c2e4a1d7d5950
waqar-ukaea <waqar.butt@ukaea.uk>
2025-08-06 18:04:44 +0100

    Added some debug prints to compare Embree ray-fire against GPRT

--- src/triangle_intersect.cpp
+++ src/triangle_intersect.cpp
@@ -57,6 +57,10 @@ void TriangleIntersectionFunc(RTCIntersectFunctionNArguments* args) {
   const PrimitiveRef& primitive_ref = user_data->prim_ref_buffer[args->primID];
 
   auto vertices = mesh_manager->face_vertices(primitive_ref.primitive_id);
+  printf("vertices: v0 = (%f, %f, %f), v1 = (%f, %f, %f), v2 = (%f, %f, %f)\n",
+         vertices[0].x, vertices[0].y, vertices[0].z,
+         vertices[1].x, vertices[1].y, vertices[1].z,
+         vertices[2].x, vertices[2].y, vertices[2].z);
 
   RTCDRayHit* rayhit = (RTCDRayHit*)args->rayhit;
   RTCDRay& ray = rayhit->ray;
