dcb9089dd7c52906177a4b4469a18334798fcf74
Patrick Shriwise <pshriwise@gmail.com>
2025-11-03 13:34:54 -0600

    Tmp removal of libmesh test for no geometry

--- tests/test_no_geom.cpp
+++ tests/test_no_geom.cpp
@@ -4,7 +4,8 @@
 
 // xdg includes
 #include "xdg/moab/mesh_manager.h"
-#include "xdg/libmesh/mesh_manager.h"
+
+// #include "xdg/libmesh/mesh_manager.h"
 
 using namespace xdg;
 
@@ -23,17 +24,17 @@ TEST_CASE("Test MOAB Mesh Without Geometry")
   REQUIRE(mesh_manager->num_volume_elements() == 8814);
 }
 
-TEST_CASE("Test libMesh Mesh Without Geometry")
-{
-  std::unique_ptr<MeshManager> mesh_manager = std::make_unique<LibMeshManager>();
+// TEST_CASE("Test libMesh Mesh Without Geometry")
+// {
+//   std::unique_ptr<MeshManager> mesh_manager = std::make_unique<LibMeshManager>();
 
-  mesh_manager->load_file("cube-mesh-no-geom.exo");
-  mesh_manager->init();
+//   mesh_manager->load_file("cube-mesh-no-geom.exo");
+//   mesh_manager->init();
 
-  // there will be two volumes: one for the cube and one for the implicit complement
-  REQUIRE(mesh_manager->num_volumes() == 2);
-  // one surface (the boundary of the mesh) separates the two volumes
-  REQUIRE(mesh_manager->num_surfaces() == 1);
+//   // there will be two volumes: one for the cube and one for the implicit complement
+//   REQUIRE(mesh_manager->num_volumes() == 2);
+//   // one surface (the boundary of the mesh) separates the two volumes
+//   REQUIRE(mesh_manager->num_surfaces() == 1);
 
-  REQUIRE(mesh_manager->num_volume_elements() == 8814);
-}
+//   REQUIRE(mesh_manager->num_volume_elements() == 8814);
+// }
