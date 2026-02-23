d189ed04d85be690320fc1599b7333064a91074f
Patrick Shriwise <pshriwise@gmail.com>
2026-02-23 11:52:22 -0600

    Including triangle surface test for MOAB transport test

--- tests/test_moab_transport.cpp
+++ tests/test_moab_transport.cpp
@@ -21,6 +21,23 @@
 using namespace xdg;
 using namespace xdg::test;
 
+TEST_CASE("Test MOAB Transport on Tris")
+{
+  std::shared_ptr<XDG> xdg = XDG::create(MeshLibrary::MOAB);
+  REQUIRE(xdg->mesh_manager()->mesh_library() == MeshLibrary::MOAB);
+  const auto& mesh_manager = xdg->mesh_manager();
+  mesh_manager->load_file("cyl-brick.h5m");
+  mesh_manager->init();
+  mesh_manager->parse_metadata();
+  xdg->prepare_raytracer();
+
+  SimulationData sim_data;
+  sim_data.xdg_ = xdg;
+  sim_data.verbose_particles_ = false;
+  sim_data.implicit_complement_is_graveyard_ = true;
+  transport_particles(sim_data);
+}
+
 TEST_CASE("Test MOAB Transport on Quads")
 {
   std::shared_ptr<XDG> xdg = XDG::create(MeshLibrary::MOAB);
