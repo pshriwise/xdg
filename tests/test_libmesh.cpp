be2a838182fae75abe10bb96d5b16e957f4d93db
Patrick Shriwise <pshriwise@gmail.com>
2026-03-23 09:21:11 -0500

    Rework libmesh ID/indexing test to use new constructor

--- tests/test_libmesh.cpp
+++ tests/test_libmesh.cpp
@@ -14,6 +14,7 @@
 #include "xdg/xdg.h"
 #include "xdg/embree/ray_tracer.h"
 
+
 using namespace xdg;
 
 void print_intersection(std::pair<double, MeshID> intersection) {
@@ -399,12 +400,14 @@ TEST_CASE("LibMesh Element ID and Index Mapping")
 
   // test mapping for non-contiguous IDs via manual modification
   {
+    // create libMesh mesh object and load the mesh
+    std::unique_ptr<libMesh::Mesh> mesh = std::make_unique<libMesh::Mesh>(*XDGConfig::config().libmesh_comm(), 3);
+    mesh->read("jezebel.exo");
+
     // now create a new mesh manager (create explicitly so we can modify the mesh before init)
-    std::unique_ptr<LibMeshManager> mesh_manager  {std::make_unique<LibMeshManager>()};
-    mesh_manager->load_file("jezebel.exo");
+    std::unique_ptr<LibMeshManager> mesh_manager = std::make_unique<LibMeshManager>(mesh.get());
 
     // tweak some of the element IDs to create gaps
-    auto* mesh = mesh_manager->mesh();
     int next_id = 0;
     std::vector<MeshID> modified_element_ids;
     for (auto* elem : mesh->active_element_ptr_range()) {
