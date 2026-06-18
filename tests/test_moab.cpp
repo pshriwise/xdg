85bfa52247e60da7cc923f2d91cb7ec4997e9416
Patrick Shriwise <pshriwise@gmail.com>
2026-06-18 13:03:30 -0500

    Better include practices for moab test file

--- tests/test_moab.cpp
+++ tests/test_moab.cpp
@@ -1,18 +1,26 @@
 // stl includes
-#include <algorithm>
-#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <map>
 #include <memory>
 #include <numeric>
 #include <random>
+#include <string>
 #include <string_view>
+#include <utility>
+#include <vector>
 
 // testing includes
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+// MOAB includes
+#include "moab/Interface.hpp"
+#include "moab/Range.hpp"
+#include "moab/Types.hpp"
+
 // xdg includes
-#include "xdg/error.h"
 #include "xdg/mesh_manager_interface.h"
 #include "xdg/moab/mesh_manager.h"
 #include "xdg/xdg.h"
