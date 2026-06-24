540db38bc3501944501efa8dfa086e8176e55cb6
Patrick Shriwise <pshriwise@gmail.com>
2026-06-24 05:10:21 -0500

    Rework quad diagonal selection

--- include/xdg/geometry/face_common.h
+++ include/xdg/geometry/face_common.h
@@ -14,19 +14,18 @@ inline Direction triangle_normal(const std::array<Vertex, 3>& vertices) {
 
 template<typename T>
 bool quad_diagonal_selection(const T& verts) {
-    auto pair_less = [](const Vertex& a0, const Vertex& a1,
-                        const Vertex& b0, const Vertex& b1) {
-    const Vertex& a_min = lower(a0, a1) ? a0 : a1;
-    const Vertex& a_max = lower(a0, a1) ? a1 : a0;
-    const Vertex& b_min = lower(b0, b1) ? b0 : b1;
-    const Vertex& b_max = lower(b0, b1) ? b1 : b0;
+  const Vertex& a0 = verts[0];
+  const Vertex& a1 = verts[2];
+  const Vertex& b0 = verts[1];
+  const Vertex& b1 = verts[3];
+  const Vertex& a_min = lower(a0, a1) ? a0 : a1;
+  const Vertex& a_max = lower(a0, a1) ? a1 : a0;
+  const Vertex& b_min = lower(b0, b1) ? b0 : b1;
+  const Vertex& b_max = lower(b0, b1) ? b1 : b0;
 
-    if (lower(a_min, b_min)) return true;
-    if (lower(b_min, a_min)) return false;
-    return lower(a_max, b_max);
-  };
-
-  return pair_less(verts[0], verts[2], verts[1], verts[3]);
+  if (lower(a_min, b_min)) return true;
+  if (lower(b_min, a_min)) return false;
+  return lower(a_max, b_max);
 }
 
 } // namespace xdg
